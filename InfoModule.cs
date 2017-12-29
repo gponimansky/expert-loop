2b77ea258d11c61e8a95092f2392ff65b46f4cd8
Guy Ponimansky <ponimansky.guy@gmail.com>
2017-12-29 11:33:23 -0500

    First Test

---
+++ InfoModule.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUpdate
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echos a message.")]
+        [Alias("repeat")]
+        public async Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync(echo);
+        }
+
+        [Command("flipcoin")]
+        [Summary("Flips coin.")]
+        [Alias("f", "flip", "flip coin")]
+        public async Task FlipCoinAsync()
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync((new Random().Next(100) >= 50) ? "Heads" : "Tails");
+   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MUpdate
+{
+    public class Program
+    {
+        private CommandService _commands;
+        private DiscordSocketClient _client;
+        private IServiceProvider _services;
+
+        public static void Main(string[] args)
+                => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _client.Log += Log;
+
+            string token = ""; // Remember to keep this private!
+
+            _services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
+
+            await InstallCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _client.MessageReceived += MessageReceived;
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.CompletedTask;
+            };
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            _client.MessageReceived += HandleCommandAsync;
+            // Discover all of the commands in this assembly and load them.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+            // Create a Command Context
+            var context = new SocketCommandContext(_client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully)
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content.ToLower() == "test")
+            {
+                await message.Channel.SendMessageAsync("It's working. " + message.Author.Mention);
+            }
+            else if(message.Content.ToLower().Contains("no u"))
+            {
+                await message.Author.SendMessageAsync("no u!");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
